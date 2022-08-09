c9bf57e1268392ab2f442da6beeaf62a9d474546
Ruan Moreira <ruaanmoreira2508@gmail.com>
2022-08-09 11:13:13 -0300

    Project Created

---
+++ api/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.json());
+const port = 3000;
+
+app.get("/teste", (req, res) => res
+    .status(200)
+    .send({ mensagem: "boas-vindas à Api" })
+);
+
+app.listen(port, () => console.log(`servidor está rodando na porta ${port}`))
+module.export = app
